ddd227898580dd75b3e95397edceb18a21a10604
Marina Lee <leemarina@gmail.com>
2024-04-08 21:19:11 -0700

    folders

---
+++ Tabletop/View/TodayMealPage.swift
@@ -0,0 +1,18 @@
+//
+//  TodayMealPage.swift
+//  Tabletop
+//
+//  Created by Marina Lee on 4/8/24.
+//
+
+import SwiftUI
+
+struct TodayMealPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodayMealPage()
+}
